004e995a7c506dba2f74652c8a942a3e8b9ac776
SiZiOUS <sizious@gmail.com>
2019-01-24 12:44:41 +0100

    Major cosmetics fixes.

--- codeblocks/src/plugins/scriptedwizard/resources/dc/templates/main_template.cpp
+++ codeblocks/src/plugins/scriptedwizard/resources/dc/templates/main_template.cpp
@@ -1,13 +1,18 @@
-/*
+/*******************************************************************************
+	Sega Dreamcast Project
+	
 	Project name : [PROJECT_NAME]
-	Created on   : [NOW] 
-*/
+	Created on   : [NOW]
+*******************************************************************************/
+
+#define PROJECT_NAME "[PROJECT_NAME]"
 
 #include <kos.h>
+[KOSLIBS_INC]
 #ifdef DEBUG
 #include <arch/gdb.h>
 #endif
-[KOSLIBS_INC]
+
 /* These macros tell KOS how to initialize itself. All of this initialization
    happens before main() gets called, and the shutdown happens afterwards. So
    you need to set any flags you want here. Here are some possibilities:
@@ -22,18 +27,24 @@
    the current KOS defaults, use INIT_DEFAULT (or leave it out entirely). */
 KOS_INIT_FLAGS(INIT_DEFAULT | INIT_MALLOCSTATS);
 
-[IF ROMDISK]/* Declare the ROMDISK */
+[IF ROMDISK]/* Declaration of the romdisk
+   You can access the files inside it by using the "/rd" mounting point. */
 extern uint8 romdisk[];
 KOS_INIT_ROMDISK(romdisk);[ENDIF ROMDISK]
 
 /* Your program's main entry point */
 int main(int argc, char *argv[]) {
 #ifdef DEBUG
+	/* This is needed for the Debug target.
+	   Please don't remove this part of code if you want to use the Code::Blocks debugger.	   
+	   Also, you'll need to configure Dreamcast Tool (dc-tool) from the DreamSDK Manager. */
 	gdb_init();
+	printf("Connection established to %s!", PROJECT_NAME);
 #endif
 
-    /* The requisite line */
-    printf("\nHello world!\n\n");
+    /* Your program start here... */
+    printf("\nHello world from %s!\n\n", PROJECT_NAME);
 
+	/* Bye... */
     return 0;
 }
