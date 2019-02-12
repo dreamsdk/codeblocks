b11a48905c5ff7aa8831de1d6c04c641e6dab98d
sizious <sizious@gmail.com>
2019-02-12 10:12:05 +0100

    Adding C::B Splash to release.cmd.

--- packager/release.cmd
+++ packager/release.cmd
@@ -15,10 +15,14 @@ set DESTINATION_DIR=%BASE_DIR%\dist
 set CB_PATCHER_FILE=%SOURCE_DIR%\codeblocks-patcher.exe
 if not exist %CB_PATCHER_FILE% goto error
 
+set CB_SPLASH_FILE=%SOURCE_DIR%\codeblocks-splash.exe
+if not exist %CB_SPLASH_FILE% goto error
+
 :release
 if not exist %DESTINATION_DIR% mkdir %DESTINATION_DIR%
 xcopy %SOURCE_DIR%\data %DESTINATION_DIR%\data\ /E
 copy %CB_PATCHER_FILE% %DESTINATION_DIR%\
+copy %CB_SPLASH_FILE% %DESTINATION_DIR%\
 copy %SOURCE_DIR%\codeblocks-backup-restore.cmd %DESTINATION_DIR%
 
 :clean
@@ -27,7 +31,7 @@ if exist %GITKEEP_FILE% del %GITKEEP_FILE%
 goto end
 
 :error
-echo Please Build the Code::Blocks Patcher binary first.
+echo Please Build the Code::Blocks Patcher/Splash binaries first.
 goto end
 
 :end
